f6db080ac8e4182e7090a7e85dc5dd30868f3f4c
Torren Martin <torren.martin89@gmail.com>
2018-01-06 21:32:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {Cake: 'Milk'};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({key: value},object);
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object = Object.assign({key: value});
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({},object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return;
+}
