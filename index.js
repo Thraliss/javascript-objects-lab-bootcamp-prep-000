deacbd60e29a9f0a5338f52f1d43e3328463d62c
Torren Martin <torren.martin89@gmail.com>
2018-01-06 21:50:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
-  return Object.assign({}, object).delete(key);
+  newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
